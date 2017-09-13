530a4c2fca83528f291a3d4fb51e934dd1200a08
Jon Bierma <jon.bierma@gmail.com>
2017-09-13 17:56:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-const app = "I don't do much."
+
+var kittens=["Milo", "Otis", "Garfield"]
+
+function destructivelyAppenKitten(kittenName){
+  kittens.push(kittenName)
+  return kittens
+}
+
+function destructivelyPrependKitten(kittenName){
+  kittens.unshift(kittenName)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(kittenName){
+  return [...kittens, kittenName]
+}
+
+function prependKitten(kittenName){
+  return [kittenName, ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(kittens.length()-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
